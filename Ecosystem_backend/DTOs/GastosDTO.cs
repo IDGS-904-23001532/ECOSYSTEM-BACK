0b7fa0a6f603c7141bfb464e88df4b5810b86020
IDGS-804-23002222 <karlamartinez2004@gmail.com>
2026-07-28 21:38:12 -0600

    Módulo Gastos

---
+++ Ecosystem_backend/DTOs/GastosDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Ecosystem_backend.DTOs
+{
+    public class RegistroGastoDto
+    {
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public int? IdProveedor { get; set; } // Nulo por si es un gasto diferente
+        public string Concepto { get; set; } = string.Empty;
+        public decimal Total { get; set; }
+    }
+
+    public class ActualizarGastoDto
+    {
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public int? IdProveedor { get; set; } // Nulo por si es un gasto diferente
+        public string Concepto { get; set; } = string.Empty;
+        public decimal Total { get; set; }
+    }
+}
