f3d32e95e4447371de92a8d69ed7d5c473c0ca02
Christopher Isaac Rivera Valderrama <christoferriveravalderrama@gmail.com>
2026-07-30 16:56:38 -0600

    fix: agregar JsonIgnore a la relacion de Cotizacion en DetalleCotizacion para evitar ciclo de referencia circular en la serializacion

--- Ecosystem_backend/Models/DetalleCotizacion.cs
+++ Ecosystem_backend/Models/DetalleCotizacion.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Ecosystem_backend.Models
 {
@@ -11,6 +12,7 @@ namespace Ecosystem_backend.Models
         [Required]
         public int IdCotizacion { get; set; }
         [ForeignKey("IdCotizacion")]
+        [JsonIgnore]
         public Cotizacion? Cotizacion { get; set; }
 
         [Required]
