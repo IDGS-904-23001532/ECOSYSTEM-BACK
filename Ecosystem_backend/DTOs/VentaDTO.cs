e86ed43a102a68f6e8217a0334525237fd7c34e4
Diego Alejandro <c4p9jyqcsm@privaterelay.appleid.com>
2026-07-29 21:07:56 -0600

    feat: Módulo Ventas 29/07/2026

---
+++ Ecosystem_backend/DTOs/VentaDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Ecosystem_backend.DTOs
+{
+    public class VentaDTO
+    {
+        public int IdCliente { get; set; }
+
+        public decimal Total { get; set; }
+
+        public string? Descripcion { get; set; }
+
+        public string MetodoPago { get; set; } = string.Empty;
+
+        public string Estado { get; set; } = "Pendiente";
+    }
+}
