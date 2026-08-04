23893be38e974dc5fad68a484e53bd8d2020c039
Diego Alejandro <c4p9jyqcsm@privaterelay.appleid.com>
2026-08-04 17:36:12 -0600

    program.cs

--- Ecosystem_backend/Program.cs
+++ Ecosystem_backend/Program.cs
@@ -6,14 +6,13 @@ using System.IO;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
@@ -27,23 +26,25 @@ app.UseSwaggerUI(c =>
 });
 
 // app.UseHttpsRedirection();
+
+// Habilita el uso de archivos estáticos por defecto
 app.UseStaticFiles();
 
-var webRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+// 1. Apuntar a la carpeta física "Uploads" en la raíz del proyecto
+var uploadsPath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
 
-if (!Directory.Exists(webRootPath))
+if (!Directory.Exists(uploadsPath))
 {
-    Directory.CreateDirectory(webRootPath);
+    Directory.CreateDirectory(uploadsPath);
 }
 
+// 2. Configurar el proveedor de archivos para exponer la carpeta "Uploads"
 app.UseStaticFiles(new StaticFileOptions
 {
-    FileProvider = new PhysicalFileProvider(webRootPath),
-    RequestPath = "" // Permite que la URL sea directa: misitio.com/Uploads/foto.png
+    FileProvider = new PhysicalFileProvider(uploadsPath),
+    RequestPath = "/Uploads" // Esto permite que la URL misitio.com/Uploads/foto.jpeg funcione
 });
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
